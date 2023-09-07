508410735f236f61703218e22d2473055ffdda78
ismail h. ozcelik <iozcelyk@gmail.com>
2023-09-07 07:19:12 +0300

    first commit

---
+++ external.js
@@ -0,0 +1,20 @@
+alert("pop-up-1");
+window.alert("pop-up-2");
+
+document.writeln("Show on the page <br/>");
+window.document.writeln("Show on the page");
+
+console.log("console log");
+console.error("console error");
+console.warn("console warn");
+console.info("console info");
+
+//single thread - only one operation at the same time
+
+var number1 = 11;
+console.log(number1);
+console.log(typeof number1);
+
+var number2 = 11.2;
+console.log(number2);
+console.log(typeof number2);
